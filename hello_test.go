caaf1b86e1447f2548e86f6b09d9ad03b6e303a5
Collin Smith <collinsmith70@gmail.com>
2019-07-15 17:23:31 -0700

    Integrated lamda for assertion

--- hello_test.go
+++ hello_test.go
@@ -3,21 +3,22 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-  t.Run("saying hello to people", func(t *testing.T) {
-    got := Hello("Collin")
-    want := "Hello, Collin"
-
+  assertMessage := func(t *testing.T, got, want string) {
+    t.Helper()
     if got != want {
       t.Errorf("got %q want %q", got, want)
     }
+  }
+
+  t.Run("saying hello to people", func(t *testing.T) {
+    got := Hello("Collin")
+    want := "Hello, Collin"
+    assertMessage(t, got, want)
   })
 
   t.Run("say 'Hello, world' when an empty string is supplied", func(t *testing.T) {
     got := Hello("")
     want := "Hello, world"
-
-    if got != want {
-      t.Errorf("got %q want %q", got, want)
-    }
+    assertMessage(t, got, want)
   })
 }
