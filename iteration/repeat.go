ab55ad421e703e6960a6a69adbdc61e6522f7889
Collin Smith <collinsmith70@gmail.com>
2019-07-15 17:54:43 -0700

    Added arg for repeat count

--- iteration/repeat.go
+++ iteration/repeat.go
@@ -1,8 +1,8 @@
 package iteration
 
-func Repeat(ch string) string {
+func Repeat(ch string, count int) string {
   var str string
-  for i := 0; i < 5; i++ {
+  for i := 0; i < count; i++ {
     str += ch
   }
   return str
