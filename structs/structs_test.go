2fa32489ec4e7e27c6b805e9217310e182e3d644
Collin Smith <collinsmith70@gmail.com>
2019-07-17 02:14:52 -0700

    Integrated table driven tests

--- structs/structs_test.go
+++ structs/structs_test.go
@@ -13,21 +13,18 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-  checkArea := func(t *testing.T, shape Shape, want float64) {
-  t.Helper()
-  got := shape.area()
-    if got != want {
-      t.Errorf("got %.2f want %.2f", got, want)
-    }
+  areaTests := []struct {
+    shape Shape
+    want  float64
+  }{
+    {Rect{12, 6}, 72.0},
+    {Circle{10}, 314.1592653589793},
   }
 
-  t.Run("rectangles", func(t *testing.T) {
-    rect := Rect{12, 6}
-    checkArea(t, rect, 72.0)
-  })
-
-  t.Run("circles", func(t *testing.T) {
-    circle := Circle{10}
-    checkArea(t, circle, 314.1592653589793)
-  })
+  for _, tt := range areaTests {
+    got := tt.shape.area()
+    if got != tt.want {
+      t.Errorf("got %.2f want %.2f", got, tt.want)
+    }
+  }
 }
